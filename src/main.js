e1e73f5c74f87a6e2cc2ce97cd4bf384fde07107
zwxs <228197155@qq.com>
2018-11-22 22:01:50 +0800

    完成了首页右侧渲染数据

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+//下载VueRouter
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+// 导入组件
+import index from "./components/index.vue";
 
 Vue.config.productionTip = false
+//导入css样式
+import "./assets/site/css/style.css"
+// 路由规则
+let routes = [{
+    path: '/',
+    redirect: '/index'
+  },
+  {
+    path:'/index',
+    component:index
 
+  }
+]
+// 实例化路由
+let router = new VueRouter({
+  routes
+});
+// 实例化Vue
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
