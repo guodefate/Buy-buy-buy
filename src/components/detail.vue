ea508566f9fd809e92166b1a0b52616cc28b3167
zwxs <228197155@qq.com>
2018-11-25 00:13:11 +0800

    完成了分类页面的数据渲染(运用了element-ui与iview两个框架)

---
+++ src/components/detail.vue
@@ -0,0 +1,229 @@
+<template>
+    <div>
+        <div class="section">
+            <div class="location">
+                <span>当前位置：</span>
+                <a href="/index.html">首页</a> &gt;
+                <a href="/goods.html">购物商城</a> &gt;
+                <a href="/goods/42/1.html">商品详情</a>
+            </div>
+        </div>
+        <div class="section">
+            <div class="wrapper clearfix">
+                <div class="wrap-box">
+                    <div class="left-925">
+                        <div class="goods-box clearfix">
+                            <div class="pic-box"></div>
+                            <div class="goods-spec">
+                                <h1>{{goodsinfo.title}}</h1>
+                                <p class="subtitle">{{goodsinfo.sub_title}}</p>
+                                <div class="spec-box">
+                                    <dl>
+                                        <dt>货号</dt>
+                                        <dd id="commodityGoodsNo">{{goodsinfo.goods_no}}</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>市场价</dt>
+                                        <dd>
+                                            <s id="commodityMarketPrice">¥{{goodsinfo.market_price}}</s>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>销售价</dt>
+                                        <dd>
+                                            <em id="commoditySellPrice" class="price">¥{{goodsinfo.sell_price}}</em>
+                                        </dd>
+                                    </dl>
+                                </div>
+                                <div class="spec-box">
+                                    <dl>
+                                        <dt>购买数量</dt>
+                                        <dd>
+                                            <div class="stock-box">
+                                                <el-input-number v-model="buyCount" :min="1" :max="goodsinfo.stock_quantity"></el-input-number>
+                                            </div>
+                                            <span class="stock-txt">
+                                                库存
+                                                <em id="commodityStockNum">{{goodsinfo.stock_quantity}}</em>件
+                                            </span>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dd>
+                                            <div id="buyButton" class="btn-buy">
+                                                <button onclick="cartAdd(this,'/',1,'/shopping.html');" class="buy">立即购买</button>
+                                                <button onclick="cartAdd(this,'/',0,'/cart.html');" class="add">加入购物车</button>
+                                            </div>
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="goodsTabs" class="goods-tab bg-wrap">
+                            <div id="tabHead" class="tab-head" style="position: static; top: 517px; width: 925px;">
+                                <ul>
+                                     <Affix>
+                                    <li>
+                                        <a href="javascript:;" @click="tabIndex=0"  :class="{selected:tabIndex==0}">商品介绍</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;" @click="tabIndex=1" :class="{selected:tabIndex==1}">商品评论</a>
+                                    </li>
+                                     </Affix>
+                                </ul>
+                            </div>
+                            <div class="tab-content entry" v-show="tabIndex==0" v-html="goodsinfo.content">
+    
+                            </div>
+                            <div class="tab-content" v-show="tabIndex==1">
+                                <div class="comment-box">
+                                    <div id="commentForm" name="commentForm" class="form-box">
+                                        <div class="avatar-box">
+                                            <i class="iconfont icon-user-full"></i>
+                                        </div>
+                                        <div class="conn-box">
+                                            <div class="editor">
+                                                <textarea id="txtContent" name="txtContent" sucmsg=" " datatype="*10-1000" nullmsg="请填写评论内容！"></textarea>
+                                                <span class="Validform_checktip"></span>
+                                            </div>
+                                            <div class="subcon">
+                                                <input id="btnSubmit" name="submit" type="submit" value="提交评论" class="submit">
+                                                <span class="Validform_checktip"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <ul id="commentList" class="list-box">
+                                        <p style="margin: 5px 0px 15px 69px; line-height: 42px; text-align: center; border: 1px solid rgb(247, 247, 247);" v-show="totalcount==0">暂无评论，快来抢沙发吧！</p>
+                                        <li 
+                                        v-for="(item, index) in comments" :key="item.id">
+                                            <div class="avatar-box">
+                                                <i class="iconfont icon-user-full"></i>
+                                            </div>
+                                            <div class="inner-box">
+                                                <div class="info">
+                                                    <span>{{item.user_name}}</span>
+                                                    <span>{{item.add_time | shortTimePlus}}</span>
+                                                </div>
+                                                <p>{{item.content}}</p>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="page-box" style="margin: 5px 0px 0px 62px;">
+                                         <!-- 使用iView的分布组件 -->
+                                            <Page :total="totalcount" show-sizer placement="top" show-elevator @on-change="pageChange" :page-size="pageSize" />
+                                      
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="left-220">
+                        <div class="bg-wrap nobg">
+                            <div class="sidebar-box">
+                                <h4>推荐商品</h4>
+                                <ul class="side-img-list">
+                                    <li v-for="(item, index) in hotgoodslist" :key="item.id">
+                                        <div class="img-box">
+                                            <router-link :to="'/detail/'+item.id">
+                                                <img :src="item.img_url">
+                                            </router-link>
+                                        </div>
+                                        <div class="txt-box">
+                                            <router-link :to="'/detail/'+item.id">{{item.title}}</router-link>
+                                            <span>{{item.add_time | shortTime}}</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <BackTop><span>🚀</span></BackTop>
+    </div>
+</template>
+<script>
+export default {
+  name: "detail",
+  data: function() {
+    return {
+      //商品的id
+      artID: "",
+      // 商品信息
+      goodsinfo: [],
+      //图片信息
+      imglist: [],
+      //热卖商品列表
+      hotgoodslist: [],
+      //购买数量
+      buyCount: 1,
+      //记录点击的tab栏索引
+      tabIndex:0,
+      //页码
+      pageIndex:1,
+    //页容量
+      pageSize:10,
+    // 评论数据
+      comments:[],
+      //评论总数
+       totalcount:0
+    };
+  },
+  methods: {
+    initData() {
+        // 初始化购买个数
+        this.buyCount=1;
+    //   console.log(this.$route.params);
+      this.artID = this.$route.params.artID;
+      this.$axios
+        .get(
+          `http://111.230.232.110:8899/site/goods/getgoodsinfo/${this.artID}`
+        )
+        .then(res => {
+        //   console.log(res);
+          this.goodsinfo = res.data.message.goodsinfo;
+          this.imglist = res.data.message.imglist;
+          this.hotgoodslist = res.data.message.hotgoodslist;
+        });
+        // 调用获取评论的方法
+        this.getComments();
+    },
+    // 获取评论数据
+    getComments(){
+        this.$axios.get(`http://111.230.232.110:8899/site/comment/getbypage/goods/${this.artID}?pageIndex=${this.pageIndex}&pageSize=${this.pageSize}`).then(res=>{
+            this.comments=res.data.message;
+            this.totalcount=res.data.totalcount;
+        })
+        
+    },
+    // 页面改变事件
+    pageChange(pageIndex){
+        this.pageIndex=pageIndex;
+        // 重新调用这一页的数据,用事情改变传过来的页码
+       this.getComments();
+    }
+
+  },
+  created() {
+    //保存数据
+    this.initData();
+  },
+  watch: {
+    $route(newVal, oldVal) {
+        this.initData();
+    }
+  }
+};
+</script>
+<style>
+.tab-content img{
+    display: block;
+    width:100%;
+}
+.ivu-back-top-show span{
+    font-size: 70px;
+    display: block;
+    transform: rotateZ(-45deg);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,21 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 导入组件
 import index from "./components/index.vue";
-
+import detail from "./components/detail.vue"
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 //导入css样式
-import "./assets/site/css/style.css"
+import "./assets/site/css/style.css";
+// 导入axios
+import axios from "axios";
+Vue.prototype.$axios= axios;
+// 导入iview
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
 // 路由规则
 let routes = [{
     path: '/',
@@ -18,12 +29,25 @@ let routes = [{
     path:'/index',
     component:index
 
+  },
+  {
+    path: '/detail/:artID',
+    component:detail
   }
 ]
 // 实例化路由
 let router = new VueRouter({
   routes
 });
+//导入moment
+import moment from "moment";
+// 全局过滤器
+Vue.filter('shortTime', value =>{ 
+    return moment(value).format("YYYY:MM:DD🛴");
+});
+Vue.filter('shortTimePlus', value =>{ 
+  return moment(value).format("YYYY-MM-DD HH:mm:ss");
+})
 // 实例化Vue
 new Vue({
   render: h => h(App),
